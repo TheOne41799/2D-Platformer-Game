421a682a1f830e172972203bdfb6fae475ad0f2e
DESKTOP-4PBISAO\Anudeep <anudeepramadugu@gmail.com>
2024-08-09 20:58:36 +0530

    Detailed Lobby screen

--- Assets/Scripts/LobbyController.cs
+++ Assets/Scripts/LobbyController.cs
@@ -11,18 +11,48 @@ public class LobbyController : MonoBehaviour
     [SerializeField] private Button playButton;
     [SerializeField] private Button quitButton;
 
+    [SerializeField] private Image levelSelectionPopupMenu;
+
+    [SerializeField] private Button level1Button;
+    [SerializeField] private Button level2Button;
+    [SerializeField] private Button level3Button;
+    [SerializeField] private Button level4Button;
+
+    //[SerializeField] private Button[] levelChangeButtons;
+
 
     private void Start()
     {
+        playButton.gameObject.SetActive(true);
+        quitButton.gameObject.SetActive(true);
+        levelSelectionPopupMenu.gameObject.SetActive(false);
+
         playButton.onClick.AddListener(PlayGame);
         quitButton.onClick.AddListener(Quit);
+
+        level1Button.onClick.AddListener(() => ChangeLevel(1));
+        level2Button.onClick.AddListener(() => ChangeLevel(2));
+        level3Button.onClick.AddListener(() => ChangeLevel(3));
+        level4Button.onClick.AddListener(() => ChangeLevel(4));
+
+        /*for (int i = 0; i < levelChangeButtons.Length; i++)
+        {
+            levelChangeButtons[i].onClick.AddListener(() => ChangeLevel(i));
+        }*/
     }
 
 
     private void PlayGame()
     {
-        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-        SceneManager.LoadScene(currentSceneIndex + 1);
+        playButton.gameObject.SetActive(false);
+        quitButton.gameObject.SetActive(false);
+        levelSelectionPopupMenu.gameObject.SetActive(true);
+    }
+
+
+    private void ChangeLevel(int levelNumber)
+    {
+        SceneManager.LoadScene(levelNumber);
     }
 
 
